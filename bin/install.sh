fe4c6b3eb1076d643f51c9306f1649e2118c911d
zeekay <zeekayy@gmail.com>
2013-02-14 15:22:01 -0800

    Update install script.

--- bin/install.sh
+++ bin/install.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-if [ "$basedir" != "$HOME/.vim" ]; then
-    if [ -d "$HOME/.vim" ]; then
-        echo "backing up ~/.vim"
-        mv $HOME/.vim $HOME/.vim.bak
-    fi
-
-    mkdir -p $HOME/.vim/addons
-    cd $HOME/.vim/addons
-    git clone https://github.com/MarcWeber/vim-addon-manager
-    git clone https://github.com/zeekay/vice
+if [ -d "$HOME/.vim" ]; then
+    echo "...backing up ~/.vim"
+    mv "$HOME/.vim" "$HOME/.vim.bak"
 fi
+
+mkdir -p "$HOME/.vim/addons"
+cd "$HOME/.vim/addons"
+git clone 'https://github.com/MarcWeber/vim-addon-manager'
+git clone 'https://github.com/zeekay/vice'
 
 echo -n "create default ~/.vimrc? (y/n) "
 read input
 if [ "$input" = "y" ]; then
     echo "...copying default vimrc"
-    cp $HOME/.vim/addons/vice/vimrc.default $HOME/.vimrc
+    cp "$HOME/.vim/addons/vice/vimrc.default" "$HOME/.vimrc"
 fi
 
 echo '...installing plugins'
-(echo; echo; echo) | vim --cmd "au BufEnter * q" -c "helptags ~/.vim/addons/vim-addon-manager/doc"
+(echo; echo; echo) | vim --cmd 'au BufEnter * q' -c 'helptags ~/.vim/addons/vim-addon-manager/doc'
 
 echo '...done!'
