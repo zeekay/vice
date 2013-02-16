e3b05a98c9106e7dfd04f944df9f85a07147a196
zeekay <zeekayy@gmail.com>
2013-02-15 17:23:15 -0800

    Update installer.

--- bin/install.sh
+++ bin/install.sh
@@ -1,37 +1,62 @@
 #!/bin/bash
 
-if [ -d "$HOME/.vim" ]; then
-    echo "...backing up ~/.vim"
+backup() {
+    original="$1"
+    backup="$original.bak"
+    name="`basename $original`"
 
-    if [ -d "$HOME/.vim.bak" ]; then
-        # append number to end of backups
-        n = 1
-        while [ -d "$HOME/.vim.bak.$n" ]; do
-            (( n ++ ))
-        done
-        mv "$HOME/.vim" "$HOME/.vim.bak.$n"
-    else
-        mv "$HOME/.vim" "$HOME/.vim.bak"
+    if [ -e "$original" ]; then
+        echo "Backing up $name"
+
+        if [ -e "$backup" ]; then
+            n=1
+            while [ -e "$backup.$n" ]; do
+                (( n ++ ))
+            done
+            backup="$backup.$n"
+        fi
+        mv "$original" "$backup"
     fi
-fi
+}
 
-echo ...creating ~/.vim and populating it with addons/ and backup/ dirs
+# backup .vim if necessary
+backup "$HOME/.vim"
+
+echo "Creating directory structure..."
 mkdir -p "$HOME/.vim/addons"
 mkdir -p "$HOME/.vim/tmp/backup"
 
-echo ...cloning vim-addon-manager and vice
+# display pretty tree if user has tree executable in path
+treebin=`which tree`
+if [ "$treebin" != "" ]; then
+    tree "$HOME/.vim"
+
+fi
+
 cd "$HOME/.vim/addons"
 git clone 'https://github.com/MarcWeber/vim-addon-manager'
 git clone 'https://github.com/zeekay/vice'
 
-echo -n 'create default ~/.vimrc? (y/n) '
-read input
+echo "Create default vimrc? (y/n) \c"
+read input </dev/tty
+
 if [ "$input" = "y" ]; then
-    echo ...copying default vimrc
+    backup "$HOME/.vimrc"
     cp "$HOME/.vim/addons/vice/example-vimrc.vim" "$HOME/.vimrc"
 fi
 
-echo ...installing plugins
 (echo; echo; echo) | vim --cmd 'au BufEnter * q' -c 'helptags ~/.vim/addons/vim-addon-manager/doc'
 
-echo ...done!
+cat << EOF
+           __
+          /  |
+ __     __vv/   _______   ______
+/  \   /  /  | /       | /      \
+vv  \ /vv/vv |/vvvvvvv/ /vvvvvv  |
+ vv  /vv/ vv |vv |      vv    vv |
+  vv vv/  vv |vv \_____ vvvvvvvv/
+   vvv/   vv |vv       |vv       |
+    v/    vv/  vvvvvvv/  vvvvvvv/
+
+...installation complete.
+EOF
